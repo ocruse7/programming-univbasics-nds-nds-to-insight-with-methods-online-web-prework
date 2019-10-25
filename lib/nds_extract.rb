90d3410d00463de0552a10bac38f46f70f3270e1
Owen Cruse <ocruse7@gmail.com>
2019-10-25 18:24:05 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,6 +12,9 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  director_data.length.times do |i|
-    
+  
+  
+  
+  
+  
 end
